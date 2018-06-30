4f8ceb55916c87dc96067bb4834a18d3af2a0618
MoeRookie <MoeRookie@163.com>
2018-06-30 09:10:31 +0800

    为选择答案的按钮添加监听器

--- app/src/main/java/com/ghsoft/geoquiz/QuizActivity.java
+++ app/src/main/java/com/ghsoft/geoquiz/QuizActivity.java
@@ -2,12 +2,31 @@ package com.ghsoft.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
-
+    private Button mTrueButton;
+    private Button mFalseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mTrueButton = findViewById(R.id.btn_true);
+        mFalseButton = findViewById(R.id.btn_false);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Does nothing yet,but soon. 什么也没做，但是很快。
+
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Does nothing yet,but soon. 什么也没做，但是很快。
+
+            }
+        });
     }
 }
