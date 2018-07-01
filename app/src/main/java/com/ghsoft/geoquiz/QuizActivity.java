298123973c22c29ba3d1da19a5f99ef81c913b56
MoeRookie <MoeRookie@163.com>
2018-07-01 18:59:53 +0800

    添加Question对象数组

--- app/src/main/java/com/ghsoft/geoquiz/QuizActivity.java
+++ app/src/main/java/com/ghsoft/geoquiz/QuizActivity.java
@@ -4,17 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_oceans,true),
+            new Question(R.string.question_mideast,false),
+            new Question(R.string.question_africa,false),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_asia,true),
+    };
+    private int mCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mQuestionTextView = findViewById(R.id.tv_question);
         mTrueButton = findViewById(R.id.btn_true);
         mFalseButton = findViewById(R.id.btn_false);
+        mNextButton = findViewById(R.id.btn_next);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
