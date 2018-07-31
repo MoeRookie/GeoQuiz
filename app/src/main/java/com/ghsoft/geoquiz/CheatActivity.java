693f0e19168df71b40c703895a2a899cc476c098
MoeRookie <MoeRookie@163.com>
2018-07-31 14:23:03 +0800

    处理低版本的兼容性问题,在低版本设备上运行时设置没有动画显示效果

--- app/src/main/java/com/ghsoft/geoquiz/CheatActivity.java
+++ app/src/main/java/com/ghsoft/geoquiz/CheatActivity.java
@@ -1,10 +1,13 @@
 package com.ghsoft.geoquiz;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -36,6 +39,33 @@ public class CheatActivity extends AppCompatActivity {
                 // 3.在showAnswer按钮的监听器代码中调用该方法
                 setAnswerShownResult(true);
                 isAnswerShown = true;
+                // 检查Andorid设备版本
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    int radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new Animator.AnimatorListener() {
+
+                        @Override
+                        public void onAnimationStart(Animator animation) {}
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {}
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {}
+                    });
+                    anim.start();
+                }else{
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
         if (savedInstanceState == null) {
